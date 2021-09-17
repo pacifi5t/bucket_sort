555cf95301a8714044332c882d96ec306016f9d4
Andrey Dergach <thequasar7@gmail.com>
2021-09-17 21:22:07 +0300

    Added random number generator function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use rand::Rng;
+
+use bucket_sort::bucket_sort;
+
 mod bucket_sort;
 
+fn generate(number: usize) -> Vec<usize> {
+    let mut rng = rand::thread_rng();
+    let mut numbers = Vec::<usize>::new();
+    for _i in 0..number {
+        numbers.push(rng.gen_range(0..u16::MAX as usize));
+    }
+
+    numbers
+}
+
 fn main() {
-    
+    let input = generate(30);
+    let sorted = bucket_sort(input, 5);
+    println!("Sorted: {:?}", sorted);
 }
